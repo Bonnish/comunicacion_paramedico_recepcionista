e3474ad26516702daf56ffc00dfcaf1c49a69ace
Bonnish <marcogajardo.jimenez@gmail.com>
2025-11-08 17:12:36 -0300

    Cambios del sistema

--- ambulancia_comms/sistema/views.py
+++ ambulancia_comms/sistema/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import hashlib
+from .models import Usuario, Paciente
 
 # Create your views here.
 def index(request):
@@ -22,5 +24,33 @@ def ver_formularios(request):
 def editar_datos(request):
     return render(request, 'editar_datos.html')
 
-def login(request):
+def mostrar_login(request):
     return render(request, 'login.html')
+
+def procesar_login(request):
+    if request.method == 'POST':
+        rut = request.POST['txtrut']
+        con = request.POST['txtpas']
+        has = hashlib.md5(con.encode('utf-8')).hexdigest()
+        comprobar = Usuario.objects.filter(rut=rut, contraseña=has).exists()
+
+        if comprobar:
+            request.session['estadoSesion'] = True
+            request.session['rutUsuario'] = rut
+            request.session['nomUsuario'] = Usuario.objects.get(rut=rut).nombre
+            cargo = request.session['cargoUsuario'] = Usuario.objects.get(rut=rut).cargo
+
+            datos = {'rut': rut, 'nombre': request.session['nomUsuario']}
+            if cargo == 'Paramedico':
+                return render(request, 'menu_paramedico.html', datos)
+            elif cargo == 'Hospital':
+                return render(request, 'menu_hospital.html', datos)
+            elif cargo == 'Administrador':
+                return render(request, 'index.html', datos)
+        else:
+            mensaje = "RUT o Contraseña incorrectos. Intente nuevamente."
+            return render(request, 'login.html', {'mensaje': mensaje})
+        
+def cerrar_sesion(request):
+    request.session.flush()
+    return render(request, 'index.html')
