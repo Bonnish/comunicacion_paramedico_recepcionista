01e403bb3fe1788459de39999e8571be2457f396
Bonnish <marcogajardo.jimenez@gmail.com>
2025-11-30 20:42:49 -0300

    Se agregó encriptacion automatica al registrar usuario desde el panel de Administracion de Django

--- ambulancia_comms/sistema/admin.py
+++ ambulancia_comms/sistema/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
 from .models import Usuario, Paciente
 
-# Register your models here.
-admin.site.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("rut", "nombre", "cargo", "email", "telefono")
+
+    def save_model(self, request, obj, form, change):
+        contraseña = form.cleaned_data.get("contraseña")
+
+        if contraseña and not contraseña.startswith("pbkdf2_"):
+            obj.contraseña = make_password(contraseña)
+
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(Paciente)
